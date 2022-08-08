578de71bf5da129e372115e2cdd66ce90612cd76
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-08-08 19:41:15 +0700

    Update y.py

--- y.py
+++ y.py
@@ -1,52 +1,8 @@
-from kivymd.app import MDApp
-from kivy.lang import Builder
+list_1 = [1, 2]
+list_2 = ["y", "u"]
+list_3 = [2, 0]
 
-
-class Test(MDApp):
-
-    def build(self):
-        self.theme_cls.primary_palette = "Gray"
-        return Builder.load_string(
-            '''
-BoxLayout:
-    orientation:'vertical'
-
-    MDToolbar:
-        title: "Demo"
-        left_action_items:[["menu",lambda x: app.navigation_draw()]]
-        right_action_items:[["logout",lambda x: app.navigation_draw()]]
-
-    MDBottomNavigation:
-        panel_color: .2, .2, .2, 1
-
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'main'
-            icon: 'home'
-
-            MDLabel:
-                text: 'main screen'
-                halign: 'center'
-
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'contacts'
-            icon: 'contacts'
-
-            MDLabel:
-                text: 'sub screen 1'
-                halign: 'center'
-
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'account'
-            icon: 'account'
-
-            MDLabel:
-                text: 'sub screen 2'
-                halign: 'center'
-'''
-        )
-
-
-Test().run()
+list = []
+for i in range(1, 4):
+    list.append(eval("list_{}".format(i)))
+print(list)
