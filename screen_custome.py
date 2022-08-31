7e4c9e6acae5a02fb8ac276fdf29cf329929479b
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-08-31 21:46:11 +0700

    Update screen_custome.py

--- screen_custome.py
+++ screen_custome.py
@@ -1,8 +1,11 @@
 # ****************kivyMD GUI **********************************
 from kivy import Config
 from kivy.graphics import Color, Rectangle
+from kivy.properties import OptionProperty, NumericProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivymd.toast import toast
 from kivymd.uix.bottomnavigation import MDBottomNavigation, MDBottomNavigationItem
+from kivymd.uix.bottomsheet import MDListBottomSheet, MDGridBottomSheet
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDFloatingActionButton
 from kivymd.uix.floatlayout import MDFloatLayout
@@ -25,13 +28,10 @@ class MainWindow(MDFloatLayout):
         toolbar = MDTopAppBar(
             title="Menu",
         )
-        # toolbar.md_bg_color = [1, 0.2, 0.2, 1]
         toolbar.left_action_items = [["menu", lambda x: print('menu')]]
         toolbar.right_action_items = [["logout", lambda x: print('exit')]]
 
         bottom = MDBottomNavigation()
-        # bottom.md_bg_color = [0.4, 0.4, 0.4, 1]
-        # bottom.panel_color = [0.2, 0.2, 0.2, 1]
         with bottom.canvas:
             Color(33 / 255, 150 / 255, 243 / 255, 0.3)
             bottom.rect = Rectangle(
@@ -69,15 +69,18 @@ class MainWindow(MDFloatLayout):
             halign='center'
         )
         button = MDFloatingActionButton(
-            icon="pencil",
+            icon="plus",
             theme_icon_color="Custom",
             md_bg_color=[1, 0, 0, 1],
             icon_color=[0, 1, 0, 1],
             pos_hint={"center_x": .85, "center_y": .2},
         )
+        button.bind(on_press=self.show_example_grid_bottom_sheet)
+
         item_1.add_widget(label_1)
         item_2.add_widget(label_2)
         item_3.add_widget(label_3)
+
         bottom.add_widget(item_1)
         bottom.add_widget(item_2)
         bottom.add_widget(item_3)
@@ -86,6 +89,35 @@ class MainWindow(MDFloatLayout):
         box.add_widget(bottom)
         self.add_widget(box)
         self.add_widget(button)
+
+    def callback_for_menu_items(self, *args):
+        # toast(args[0])
+        print(args[0])
+
+    def show_example_grid_bottom_sheet(self, x):
+        bottom_sheet_menu = MDListBottomSheet(
+            radius=10,
+            radius_from='top'
+        )
+        # bottom_sheet_menu = MDGridBottomSheet(
+        #     radius=10,
+        #     radius_from='top'
+        # )
+        data = {
+            "Facebook": "facebook",
+            "YouTube": "youtube",
+            "Twitter": "twitter",
+            "Da Cloud": "cloud-upload",
+            "Camera": "camera",
+        }
+        for item in data.items():
+            bottom_sheet_menu.add_item(
+                item[0],
+                lambda x, y=item[0]: self.callback_for_menu_items(y),
+                icon=item[1],
+                # icon_src=item[1],
+            )
+        bottom_sheet_menu.open()
 
 
 class MyApp(MDApp):
