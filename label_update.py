90ce63a7ec8c9d2a0a15f41df6561b650fea2971
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-10-29 20:20:49 +0700

    Update label_update.py

--- label_update.py
+++ label_update.py
@@ -11,8 +11,8 @@ class Test(MDApp):
     def __init__(self):
         super().__init__()
         self.textedit = None
-        self.update_signal = None
         self.count = 0
+        self.update_signal = None
         self.label = None
         self.list = ["0s", "1s", "2d", "3a", "4y", "5s", "6d", "7a", "8y"]
 
@@ -27,45 +27,30 @@ class Test(MDApp):
             hint_text="Round mode",
             mode="round",
             max_text_length=15,
-            helper_text="Massage"
         )
         button = MDRaisedButton(
             text="MDRaisedButton",
             md_bg_color="red",
         )
-        button.bind(on_press=self.hello)
-        button_1 = MDRaisedButton(
-            text="MDRaisedButton_1",
-            md_bg_color="red",
-        )
-        button_1.bind(on_press=self.hello_1)
-
+        button.bind(on_press=self.active_)
         screen.add_widget(layout)
         layout.add_widget(self.label)
         layout.add_widget(self.textedit)
         layout.add_widget(button)
-        layout.add_widget(button_1)
         return screen
 
-    def hello(self, r):
-        self.update_signal = Clock.schedule_interval(self.tt, 1)
+    def active_(self, x):
+        self.update_signal = Clock.schedule_interval(self.update, 1)
 
-    def hello_1(self, r):
-        self.update_signal.cancel()
-
-    def tt(self, x):
-        self.count += 1
-        print(self.count)
-        self.label.text = self.list[self.count]
-        if self.count == 8:
-            self.update_signal.cancel()
-
-        # a = self.textedit.text
-        # if a == "e":
+    def update(self, x):
+        # self.label.text = self.list[self.count]
+        # self.count += 1
+        # if self.count == 9:
         #     self.update_signal.cancel()
-        # else:
-        #     print(a)
-        #     self.label.text = str(a)
+
+        text = self.textedit.text
+        print(text)
+        self.label.text = str(text)
 
 
 Test().run()
